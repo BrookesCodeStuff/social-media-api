42db0ddc88616a1d3b2f2949af1fdb26528ccbc7
Brooke <brookepaglia@gmail.com>
2022-04-23 15:54:37 -0400

    add user API routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,39 +1,41 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
-new ReactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: "A reaction is required!",
+      max: [280, "Max chars. 280"],
+    },
+    username: {
+      type: String,
+      required: "Username is required!",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  reactionBody: {
-    type: String,
-    required: 'A reaction is required!',
-    max: [280, 'Max chars. 280'],
-  },
-  username: {
-    type: String,
-    required: 'Username is required!'
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal)
+  {
+    toJSON: {
+      getters: true,
+    },
   }
-},
-{
-  toJSON: {
-    getters: true
-  }
-})
+);
 
-new ThoughtSchema = new Schema(
+const ThoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      required: 'A thought is required!',
-      min: [1, 'Thought must be at least 1 character long.'],
-      maxlength: [280, 'Thought must be no longer than 280 characters.']
+      required: "A thought is required!",
+      min: [1, "Thought must be at least 1 character long."],
+      maxlength: [280, "Thought must be no longer than 280 characters."],
     },
     createdAt: {
       type: Date,
@@ -42,23 +44,23 @@ new ThoughtSchema = new Schema(
     },
     username: {
       type: String,
-      required: 'Username is required.'
+      required: "Username is required.",
     },
-    reactions: [ReactionSchema]
+    reactions: [ReactionSchema],
   },
-    {
+  {
     toJSON: {
       getters: true,
       virtuals: true,
     },
     id: false,
   }
-)
+);
 
-ThoughtSchema.virtual('reactionCount').get(function() {
+ThoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
-})
+});
 
-const Thought = model('Thought', ThoughtSchema);
+const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;
